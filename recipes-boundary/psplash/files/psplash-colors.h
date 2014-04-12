9b3221b2a969ac05a0d667a67f6a02d9bf50d49a
Mahyar Yaghmaee <mahyar@boundarydevices.com>
2014-04-11 17:56:14 -0700

    customize splash bar

--- recipes-boundary/psplash/files/psplash-colors.h
+++ recipes-boundary/psplash/files/psplash-colors.h
@@ -20,15 +20,15 @@
 #define _HAVE_PSPLASH_COLORS_H
 
 /* This is the overall background color */
-#define PSPLASH_BACKGROUND_COLOR 0xDD,0xDD,0xDD
+#define PSPLASH_BACKGROUND_COLOR 0x00,0x27,0x54
 
 /* This is the color of any text output */
-#define PSPLASH_TEXT_COLOR 0x00,0x00,0x00
+#define PSPLASH_TEXT_COLOR 0xDD,0xDD,0xDD
 
 /* This is the color of the progress bar indicator */
-#define PSPLASH_BAR_COLOR 0x00,0x00,0x00
+#define PSPLASH_BAR_COLOR 0xFF,0xFF,0xFF
 
 /* This is the color of the progress bar background */
-#define PSPLASH_BAR_BACKGROUND_COLOR 0xDD,0xDD,0xDD
+#define PSPLASH_BAR_BACKGROUND_COLOR 0x00,0x27,0x54
 
 #endif
--- recipes-boundary/psplash/psplash_git.bbappend
+++ recipes-boundary/psplash/psplash_git.bbappend
@@ -1,3 +1,13 @@
 FILESEXTRAPATHS_prepend_poky := "${THISDIR}/files:"
 
-SRC_URI += "file://psplash-colors.h "
+SRC_URI += "file://psplash-colors.h \
+	    file://psplash-bar-img.h "
+
+do_configure_append () {
+        cd ${S}
+        cp ../psplash-colors.h ./
+
+        cp ../psplash-bar-img.h ./psplash-bar-img.h
+}
+
+
