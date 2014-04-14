aa237a4844998b7f2ea7c05f71e12255d42ce787
Mahyar Yaghmaee <mahyar@boundarydevices.com>
2014-04-14 16:19:53 -0700

    default:google.com

--- recipes-boundary/boundary-image-browser/chrome-launch/10xstartchrome.sh
+++ recipes-boundary/boundary-image-browser/chrome-launch/10xstartchrome.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-export CHROME_DEVEL_SANDBOX=/usr/sbin/chrome-devel-sandbox && /usr/bin/chrome/chrome --user-data-dir --kiosk
+export CHROME_DEVEL_SANDBOX=/usr/sbin/chrome-devel-sandbox && /usr/bin/chrome/chrome www.google.com --user-data-dir --kiosk
